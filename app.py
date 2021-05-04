789f078372240b6e3bb77bf1a0fd4425ce78ef7e
Arjun Pillai <arjun.pillai.08@gmail.com>
2021-05-04 09:45:27 -0400

    Changes

--- app.py
+++ app.py
@@ -1,46 +1,65 @@
-from flask import Flask, redirect, jsonify
-from db_auth import connect_to_db
-import pymongo, ssl, os
-from dotenv import load_dotenv
+import flask
+from flask import request
+import os
 import json
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+import re
+from flask_cors import CORS
+from db_auth import connect_to_db
 
 """
 """
+app = flask.Flask(__name__)
+CORS(app)
 
-collection = connect_to_db()
+db = connect_to_db()
 #all_instances = collection.find()#
-app = Flask(__name__)
 
 port = 4001
 host = "0.0.0.0"
 
-@app.route("/internships")
-def home():
-    all_instances = list(collection.find({}))
-    new_list = list()
-    counter = 0
-    for i in all_instances:
-        dictionary = all_instances[counter]
-        dictionary.pop("_id", None)
-        new_list.append(dictionary)
-        counter += 1
-    print(len(new_list))
-    internships = jsonify(new_list)
-    return internships
+@app.route('/',methods = ['GET'])
+def get_data():
 
-@app.route("/internships/remote")
-def remote():
-    my_query = {"is_remote": True}
-    all_instances = list(collection.find(my_query, {"_id": 0}))
-    return jsonify(all_instances)
+    qs = request.args.get('name')
+    if qs is None:
+        todos = db.internships.find()
+        return dumps(todos)
+    else:
+        rgx = re.compile(qs, re.IGNORECASE)  # compile the regex
+        todos = db.internships.find({'company':rgx})
+        return dumps(todos)
 
-@app.route("/internships/industry/<name>")
-def in_person(name):
-    name = name.capitalize()
-    my_query = {"industry": str(name)}
-    all_instances = list(collection.find(my_query, {"_id": 0}))
-    print(all_instances)
-    return jsonify(all_instances)
+@app.route('/filter',methods = ['POST'])
+def filter_data():
+    print(request.json)
+    industry_name = request.json['industry']
+    is_paid = request.json['paid']
+    is_remote = request.json['remote']
+    position = request.json['position']
 
-if __name__ == "__main__":
-    app.run()
+    industry_name_rgx = re.compile(industry_name, re.IGNORECASE)
+    position_rgx = re.compile(position, re.IGNORECASE)
+
+    if industry_name == '':
+        todos = db.internships.find({'is_paid': {"$in":is_paid}, 'is_remote': {"$in":is_remote}, 'position': position_rgx})
+        return dumps(todos)
+    else:
+        todos = db.internships.find({'industry':industry_name_rgx, 'is_paid': {"$in":is_paid}, 'is_remote': {"$in":is_remote}, 'position': position_rgx})
+        return dumps(todos)
+    
+@app.route('/industry',methods = ['GET'])
+def get_industry():
+
+    # Get All industry type
+    todos = db.internships.find({}, {'industry': 1, '_id': 0})
+
+    # Filter duplicate industry values
+    todos = [dict(t) for t in {tuple(d.items()) for d in todos}]
+
+    # Make list of industries and send response
+    return dumps(filter(None, [d['industry'] for d in todos]))
+    
+if __name__ == '__main__':
+    app.run()    
